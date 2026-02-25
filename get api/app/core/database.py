66286d27cf7c9b44cac86d5479fc947c580bd7c2
Madhubala kumari <kumarimadhubala822@gmail.com>
2026-02-25 18:40:49 +0530

    api curd
    api curd

--- get api/app/core/database.py
+++ get api/app/core/database.py
@@ -5,6 +5,18 @@ DATABASE_URL = "postgresql://postgres:postgre@localhost:5432/department_db"
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- get api/app/main.py
+++ get api/app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 from app.api import department_api
-from app.core.database import engine, Base
-from app.Entities.department_model import Department
 
 app = FastAPI()
 
-# Create tables in database
-Base.metadata.create_all(bind=engine)
 
 # Include API router
 app.include_router(department_api.router)
