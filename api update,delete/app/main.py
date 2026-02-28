f8d35e5640a04f06f38ea5e56503a33a4f736642
Madhubala kumari <kumarimadhubala822@gmail.com>
2026-02-28 21:23:18 +0530

    removed

--- api update,delete/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api import department
-
-app = FastAPI()
-
-app.include_router(department.router)
--- app/core/database.py
+++ app/core/database.py
@@ -1,12 +1,18 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
 from app.core.config import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True
+)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     expire_on_commit=False
 )
+
+Base = declarative_base()
 
 async def get_db():
     async with AsyncSessionLocal() as session:
