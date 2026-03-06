0e371ffe0c2d0848be4eb1fc97debf624012c66c
Madhubala kumari <kumarimadhubala822@gmail.com>
2026-03-06 15:53:57 +0530

    Fix Database Dependency
    Used get_db from core database file for proper DB session management.

--- app/api/department_api.py
+++ app/api/department_api.py
@@ -1,18 +1,17 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.dtos.department_dto import department_dto
+
 router = APIRouter(prefix="/Department", tags=["Department Master"])
+
 
 @router.post("/Create")
 async def create_department(
     state: department_dto,
-    db: AsyncSession = Depends(get_db)
+    db: Session = Depends(get_db)
 ):
- 
-
+    
     return {
         "message": "State created successfully"
-         
     }
- 
